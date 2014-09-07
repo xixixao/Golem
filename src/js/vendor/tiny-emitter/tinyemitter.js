4edcaeb321a52adef52469be3560cc4b8345e276
xixixao <xixixao@seznam.cz>
2014-09-07 00:34:04 -0400

    Updated tinyemitter

--- src/js/vendor/tiny-emitter/tinyemitter.js
+++ src/js/vendor/tiny-emitter/tinyemitter.js
@@ -1,17 +1,18 @@
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.TinyEmitter=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function E () {
-  this.e = {};
+  // Keep this empty so it's easier to inherit from
+  // (via https://github.com/lipsmack from https://github.com/scottcorgan/tiny-emitter/issues/3)
 }
 
 E.prototype = {
   on: function (name, callback, ctx) {
-    var e = this.e;
-    
+    var e = this.e || (this.e = {});
+
     (e[name] || (e[name] = [])).push({
       fn: callback,
       ctx: ctx
     });
-    
+
     return this;
   },
 
@@ -21,37 +22,42 @@ E.prototype = {
       self.off(name, fn);
       callback.apply(ctx, arguments);
     };
-    
+
     return this.on(name, fn, ctx);
   },
 
   emit: function (name) {
     var data = [].slice.call(arguments, 1);
-    var evtArr = (this.e[name] || []).slice();
-    var i;
-    
-    for (i in evtArr) {
+    var evtArr = ((this.e || (this.e = {}))[name] || []).slice();
+    var i = 0;
+    var len = evtArr.length;
+
+    for (i; i < len; i++) {
       evtArr[i].fn.apply(evtArr[i].ctx, data);
     }
-    
+
     return this;
   },
 
   off: function (name, callback) {
-    var e = this.e;
+    var e = this.e || (this.e = {});
     var evts = e[name];
-    var i;
-    
+    var liveEvents = [];
+
     if (evts && callback) {
-      for (i in evts) {
-        if (evts[i].fn !== callback) continue;
-        evts.splice(i, 1);
+      for (var i = 0, len = evts.length; i < len; i++) {
+        if (evts[i].fn !== callback) liveEvents.push(evts[i]);
       }
     }
-    else if (evts) {
-      e[name] = [];
-    }
-    
+
+    // Remove event from queue to prevent memory leak
+    // Suggested by https://github.com/lazd
+    // Ref: https://github.com/scottcorgan/tiny-emitter/commit/c6ebfaa9bc973b33d110a84a307742b7cf94c953#commitcomment-5024910
+
+    (liveEvents.length)
+      ? e[name] = liveEvents
+      : delete e[name];
+
     return this;
   }
 };
