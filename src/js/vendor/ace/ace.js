1bb77c2763ae96d5395b206dc8a24d19634d1d9d
xixixao <xixixao@seznam.cz>
2014-09-22 01:14:59 +0200

    Remove the window.require which breaks optimization

--- src/js/vendor/ace/ace.js
+++ src/js/vendor/ace/ace.js
@@ -18146,12 +18146,3 @@ exports.EditSession = EditSession;
 exports.UndoManager = UndoManager;
 });
 ;
-            (function() {
-                window.require(["ace/ace"], function(a) {
-                    a && a.config.init(true);
-                    if (!window.ace)
-                        window.ace = a;
-                    for (var key in a) if (a.hasOwnProperty(key))
-                        window.ace[key] = a[key];
-                });
-            })();
