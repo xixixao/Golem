c0fd19c77e6633d74d44aaca3f53315079fc62cc
xixixao <xixixao@seznam.cz>
2015-03-02 17:01:26 +0000

    Made tiny emmitter async

--- src/js/vendor/tiny-emitter/tinyemitter.js
+++ src/js/vendor/tiny-emitter/tinyemitter.js
@@ -29,12 +29,14 @@ E.prototype = {
   emit: function (name) {
     var data = [].slice.call(arguments, 1);
     var evtArr = ((this.e || (this.e = {}))[name] || []).slice();
-    var i = 0;
     var len = evtArr.length;
 
-    for (i; i < len; i++) {
-      evtArr[i].fn.apply(evtArr[i].ctx, data);
-    }
+    setTimeout(function () {
+      var i = 0;
+      for (i; i < len; i++) {
+          evtArr[i].fn.apply(evtArr[i].ctx, data);
+      }
+    }, 0);
 
     return this;
   },
